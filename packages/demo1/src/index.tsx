cfcfe177b81254c6a43c96aeff9bc51585a038e5
唐硕 <shuo.tang@mijahealth.com>
2023-02-06 11:05:05 +0800

    feat: add

--- packages/demo1/src/index.tsx
+++ packages/demo1/src/index.tsx
@@ -4,15 +4,19 @@
  * @Author: tangshuo
  * @Date: 2023-01-30 14:42:38
  * @LastEditors: tangshuo
- * @LastEditTime: 2023-02-02 15:46:14
+ * @LastEditTime: 2023-02-06 10:58:28
  */
-import React from 'react';
+import React, { useState } from 'react';
 import './index.less';
 
 const Demo = () => {
+  const [boolean, setBoolean] = useState('');
   return (
     <div className="demo">
-      2gdg<span className="child">rerdddddd</span>
+      2gdg
+      <span className="child" onClick={() => setBoolean('333')}>
+        {boolean ?? 'rerdddddd'}
+      </span>
     </div>
   );
 };
