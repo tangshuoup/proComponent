484fad1c7d15848943b85893b2204e144c2b08bf
唐硕 <shuo.tang@mijahealth.com>
2023-02-09 15:54:16 +0800

    feat: add

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -4,6 +4,7 @@
  * @Author: tangshuo
  * @Date: 2023-01-31 14:39:38
  * @LastEditors: tangshuo
- * @LastEditTime: 2023-02-02 09:43:50
+ * @LastEditTime: 2023-02-09 15:48:22
  */
 export * from '@tangshuo/pro-demo1';
+export * from '@tangshuo/pro-demo2';
